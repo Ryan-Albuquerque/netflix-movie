ee1c2ea607e27d11d724a6f60526f4de3d1563fe
Ryan Albuquerque <56835754+Ryan-Albuquerque@users.noreply.github.com>
2022-01-19 00:52:00 -0400

    Listen port before initialization of pkgs

--- index.js
+++ index.js
@@ -7,13 +7,13 @@ const routesConfig = require('./routesConfig');
 
 const app = express();
 
+app.listen('8080', async () => {
+  await db.connect();
+  console.log('API is running in 8080!');
+});
+
 app.use(cors());
 app.use(helmet());
 app.use(express.json());
 
 app.use(routesConfig);
-
-app.listen('8080', async () => {
-  await db.connect();
-  console.log('API is running in 8080!');
-});
